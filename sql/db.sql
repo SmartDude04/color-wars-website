2ea644c95098ea5135578073872a25d0e779fb05
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-07-07 19:19:45 -0400

    Refactor SQL database structure and add new tables
    The code refactor involves restructuring the existing SQL database by prefixing table names with the database name for better organization. Additionally, three new tables, 'teams', 'groups', and 'points', are added to increase relational data representation, with each table including necessary keys, data, and constraint references.

--- sql/db.sql
+++ sql/db.sql
@@ -1,18 +1,12 @@
 CREATE DATABASE `color-wars`;
 
-create table `groups`
-(
-    grp_name varchar(32) not null,
-    grp_team varchar(32) not null
-);
-
-create table pending
+create table `color-wars`.pending
 (
     pnd_usr_name     varchar(32)  not null,
     pnd_usr_password varchar(256) not null
 );
 
-create table sessions
+create table `color-wars`.sessions
 (
     sn_username          varchar(32)  not null,
     sn_series_identifier varchar(64)  not null,
@@ -20,13 +14,37 @@ create table sessions
     sn_expire            int unsigned not null
 );
 
-create table teams
+create table `color-wars`.teams
 (
-    tm_color  varchar(32) not null,
-    tm_points int         not null
+    tm_id        int auto_increment
+        primary key,
+    tm_name      varchar(32) not null,
+    tm_hex_color varchar(12) not null
 );
 
-create table users
+create table `color-wars`.`groups`
+(
+    grp_id    int auto_increment
+        primary key,
+    grp_name  varchar(32) not null,
+    grp_tm_id int         not null,
+    constraint groups_teams_tm_id_fk
+        foreign key (grp_tm_id) references `color-wars`.teams (tm_id)
+);
+
+create table `color-wars`.points
+(
+    pts_timestamp datetime not null,
+    pts_tm_id     int      not null,
+    pts_grp_id    int      not null,
+    pts_amount    int      not null,
+    constraint points_groups_grp_id_fk
+        foreign key (pts_grp_id) references `color-wars`.`groups` (grp_id),
+    constraint points_teams_tm_id_fk
+        foreign key (pts_tm_id) references `color-wars`.teams (tm_id)
+);
+
+create table `color-wars`.users
 (
     usr_id       int auto_increment
         primary key,
@@ -34,10 +52,11 @@ create table users
     usr_password varchar(256) not null
 );
 
-create table roles
+create table `color-wars`.roles
 (
     rl_usr_id int not null,
     rl_role   int not null,
     constraint roles_users_usr_id_fk
-        foreign key (rl_usr_id) references users (usr_id)
+        foreign key (rl_usr_id) references `color-wars`.users (usr_id)
 );
+
