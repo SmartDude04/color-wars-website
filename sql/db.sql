a64d14fbfc89b1766602dcf0fff5f81aa1fa3066
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-07-10 20:49:49 -0400

    Update sql ddl

--- sql/db.sql
+++ sql/db.sql
@@ -1,12 +1,11 @@
 CREATE DATABASE `color-wars`;
 
-create table `color-wars`.pending
+create table pending
 (
-    pnd_usr_name     varchar(32)  not null,
-    pnd_usr_password varchar(256) not null
+    pnd_usr_name varchar(32) not null
 );
 
-create table `color-wars`.sessions
+create table sessions
 (
     sn_username          varchar(32)  not null,
     sn_series_identifier varchar(64)  not null,
@@ -14,7 +13,7 @@ create table `color-wars`.sessions
     sn_expire            int unsigned not null
 );
 
-create table `color-wars`.teams
+create table teams
 (
     tm_id        int auto_increment
         primary key,
@@ -22,29 +21,29 @@ create table `color-wars`.teams
     tm_hex_color varchar(12) not null
 );
 
-create table `color-wars`.`groups`
+create table `groups`
 (
     grp_id    int auto_increment
         primary key,
     grp_name  varchar(32) not null,
     grp_tm_id int         not null,
     constraint groups_teams_tm_id_fk
-        foreign key (grp_tm_id) references `color-wars`.teams (tm_id)
+        foreign key (grp_tm_id) references teams (tm_id)
 );
 
-create table `color-wars`.points
+create table points
 (
     pts_timestamp datetime not null,
     pts_tm_id     int      not null,
     pts_grp_id    int      not null,
     pts_amount    int      not null,
     constraint points_groups_grp_id_fk
-        foreign key (pts_grp_id) references `color-wars`.`groups` (grp_id),
+        foreign key (pts_grp_id) references `groups` (grp_id),
     constraint points_teams_tm_id_fk
-        foreign key (pts_tm_id) references `color-wars`.teams (tm_id)
+        foreign key (pts_tm_id) references teams (tm_id)
 );
 
-create table `color-wars`.users
+create table users
 (
     usr_id       int auto_increment
         primary key,
@@ -52,11 +51,10 @@ create table `color-wars`.users
     usr_password varchar(256) not null
 );
 
-create table `color-wars`.roles
+create table roles
 (
     rl_usr_id int not null,
     rl_role   int not null,
     constraint roles_users_usr_id_fk
-        foreign key (rl_usr_id) references `color-wars`.users (usr_id)
+        foreign key (rl_usr_id) references users (usr_id)
 );
-
