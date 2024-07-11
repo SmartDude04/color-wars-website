0cd2a8487d28bab9be64a41b6b51d71d813508fd
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-07-10 21:20:49 -0400

    Add new user success page and corresponding CSS
    Implemented a new page to show upon successful user creation, including an informational message about pending account verification. The changes also involve the addition of a matching CSS file that is responsive to both landscape and portrait display orientations.

---
+++ public/login/new-user.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Log In &bull; GCG Color Wars</title>
+    <link rel="icon" type="image/x-icon" href="../img/favicon.ico">
+    <link rel="stylesheet" href="../css/login/user-created.css">
+    <link rel="stylesheet" href="../css/fonts.css">
+</head>
+<body>
+<div class="window">
+    <div class="texts">
+        <h1 class="main-text">Success!</h1>
+        <h1 class="subtext">Your account has been created and is awaiting verification from an administrator.</h1>
+    </div>
+
+    <a class="btn-link" href="../index.php">
+        <button class="btn">OK</button>
+    </a>
+
+</div>
+</body>
+</html>
