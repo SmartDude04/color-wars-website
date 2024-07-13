fa0368bf2ca649c732de0a0d90a7ab01d1201114
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-07-13 19:56:00 -0400

    Add files for history tab

---
+++ api/history/get-points-history.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once "../api/history/get-points-history.php";
+
+
+function get_points_history(): array {
+    $conn = db_connect();
+
+    $result = $conn->execute_query("SELECT pts_id, tm_name AS 'team', pts_timestamp AS 'date', grp_name AS 'group', usr_name AS 'user', pts_amount AS 'amount'
+                                    FROM `points`
+                                    JOIN teams ON pts_tm_id = tm_id
+                                    JOIN `groups` ON pts_grp_id = grp_id
+                                    JOIN users ON pts_usr_id = usr_id
+                                    ORDER BY `pts_timestamp`");
+
+    $return_array = array();
+
+    // Make the date for each row into a more readable format
+    while ($row = $result->fetch_assoc()) {
+        $date_arr = date_parse($row['date']);
+
+        // Make sure the times stay as two digits
+        $date_arr["hour"] = intval($date_arr["hour"]) < 10 ? "0" . $date_arr["hour"] : $date_arr["hour"];
+        $date_arr["minute"] = intval($date_arr["minute"]) < 10 ? "0" . $date_arr["minute"] : $date_arr["minute"];
+
+        // Add an AM or PM depending on the hour
+        $ampm = "AM";
+        if (intval($date_arr["hour"]) > 12) {
+            $date_arr["hour"] = strval(intval($date_arr["hour"]) - 12);
+            $ampm = "PM";
+        }
+
+        $new_date = $date_arr["hour"] . ":" . $date_arr["minute"] . " " . $ampm . " on " . $date_arr["month"] . "/" . $date_arr["day"];
+
+        $return_array[] = $row;
+        $return_array[array_key_last($return_array)]['date'] = $new_date;
+    }
+
+    return $return_array;
+}
---
+++ public/home/history.php
@@ -0,0 +1,64 @@
+<div class="history-container">
+    <table class="history-table">
+
+        <tr class="history-table-header">
+            <th class="team-header">Team</th>
+            <th class="mobile-disabled date-header">Date/Time</th>
+            <th class="mobile-disabled group-header">Group</th>
+            <th class="mobile-disabled user-header">User</th>
+            <th class="amount-header">Amount</th>
+            <?php
+
+            // If the user is an admin, display the edit column header
+            try {
+                if (confirm_session() && $_SESSION["role"] == 2) {
+                    echo "<th class='edit-header'></th>";
+                }
+            } catch (\Random\RandomException $e) {
+            }
+            ?>
+
+        </tr>
+
+        <!--<tr>
+            <td>10</td>
+            <td class="mobile-disabled">10</td>
+            <td class="mobile-disabled">10</td>
+            <td class="mobile-disabled">10</td>
+            <td>10</td>
+            <td class="edit-cell">
+                <a href="edit/edit.php"><img src="img/edit.png" onmouseover="this.src='img/edit-hover.png'" onmouseout="this.src='img/edit.png'" alt="Edit"></a>
+            </td>
+        </tr>-->
+
+        <?php
+
+        require_once "../api/history/get-points-history.php";
+
+        $rows = get_points_history();
+
+        // Make each table row
+
+        foreach ($rows as $row) {
+            $admin = false;
+            try {
+                $admin = confirm_session() && $_SESSION['role'] == 2;
+            } catch (\Random\RandomException $e) {
+            }
+            echo "<tr>";
+            echo "<td>" . ucwords($row['team']) . "</td>";
+            echo "<td class='mobile-disabled'>" . $row['date'] . "</td>";
+            echo "<td class='mobile-disabled'>" . ucwords($row['group']) . "</td>";
+            echo "<td class='mobile-disabled'>" . ucwords($row['user']) . "</td>";
+            echo "<td>" . number_format($row['amount']) . "</td>";
+
+            // If the user has high enough perms, show them the edit button
+            if ($admin) {
+                echo "<td class='edit-cell'><a href='edit-history/edit.php?id=" . $row["pts_id"] . "'><img src='img/edit.png' onmouseover='this.src=`img/edit-hover.png`' onmouseout='this.src=`img/edit.png`' alt='Edit'></a></td>";
+            }
+
+            echo "</tr>";
+
+        }
+        ?>
+</div>
