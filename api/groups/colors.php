7cf4a33d918153f4437ba1fac9b6fe5f5a89ec43
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-07-13 12:13:15 -0400

    Add API files for the homepage

---
+++ api/groups/colors.php
@@ -0,0 +1,46 @@
+<?php
+
+function lighten($hex): string {
+    $red = hexdec(substr($hex, 0, 2)) + 0x80;
+    $red = dechex(min($red, 0xff));
+
+    $green = hexdec(substr($hex, 2, 2)) + 0x80;
+    $green = dechex(min($green, 0xff));
+
+    $blue = hexdec(substr($hex, 4, 2)) + 0x80;
+    $blue = dechex(min($blue, 0xff));
+
+    return $red . $green . $blue;
+}
+
+function darken($hex): string {
+    $red = hexdec(substr($hex, 0, 2)) - 0x80;
+    $red = dechex(max($red, 0x00));
+    $red = $red == "0" ? "00" : $red;
+
+    $green = hexdec(substr($hex, 2, 2)) - 0x80;
+    $green = dechex(max($green, 0x00));
+    $green = $green == "0" ? "00" : $green;
+
+
+    $blue = hexdec(substr($hex, 4, 2)) - 0x80;
+    $blue = dechex(max($blue, 0x00));
+    $blue = $blue == "0" ? "00" : $blue;
+
+    return $red . $green . $blue;
+}
+
+function make_css_gradient($hex): string {
+    $light = lighten($hex);
+    $dark = darken($hex);
+
+    return "background-image: linear-gradient(135deg, #$light, #$dark)";
+}
+
+function make_rgba($hex, $opacity): string {
+    $r = hexdec(substr($hex, 0, 2));
+    $g = hexdec(substr($hex, 2, 2));
+    $b = hexdec(substr($hex, 4, 2));
+
+    return "rgba($r, $g, $b, $opacity)";
+}
---
+++ api/groups/group-arrays.php
@@ -0,0 +1,27 @@
+<?php
+require_once "../api/login/authentication.php";
+
+
+function get_group_arrays(): array {
+    $conn = db_connect();
+
+    $result = $conn->query("SELECT tm_id, tm_name, SUM(pts_amount) AS `total`, tm_hex_color FROM teams JOIN points ON tm_id = pts_tm_id GROUP BY pts_tm_id");
+    $return_arr = array();
+
+    while ($row = $result->fetch_assoc()) {
+        $return_arr[] = $row;
+
+        // Add groups to that array
+        $groups_result = $conn->query("SELECT grp_name FROM `groups` WHERE grp_tm_id = '" . $row["tm_id"] . "'");
+        $groups_arr = array();
+
+        while ($row_group = $groups_result->fetch_assoc()) {
+            $groups_arr[] = ucfirst($row_group["grp_name"]);
+        }
+
+        $return_arr[array_key_last($return_arr)]["groups"] = $groups_arr;
+    }
+
+    
+    return $return_arr;
+}
