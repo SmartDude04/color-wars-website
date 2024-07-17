fa361febc8f19dd929a24a3fd06dfbc08f7bbc2c
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-07-16 21:01:52 -0400

    Add about page to the website

---
+++ public/home/about.php
@@ -0,0 +1,24 @@
+<div class="view">
+    <img src="img/color-wars-title.png" alt="Logo" class="logo">
+    <p class="text">
+        As I was looking for a coding project to keep up my skills over the summer, I thought of creating a
+        website to keep track of color wars scores. After realizing a fully-functional website would be quite
+        a challenge, I  quickly got to work. Thankfully, I can say (with some bias) that I have succeeded in creating
+        a website that can help facilitate all that is needed to manage color wars at CGC: viewing points, managing points,
+        managing colors and groups, and storing user accounts for those who need it. Aside from being fun to make, my
+        hope is that this website can make the management, accuracy, and viewing of color wars material for all who
+        want to do so much easier.
+        <br/>
+        <br/>
+        &emsp;&emsp;- Aidan O'Connor, Creator
+    </p>
+
+    <p class="text">
+        As this is still an ongoing project, issues may arise. If you encounter an issue, bug, or other general
+        problem, please contact me so I can get it resolved quickly.
+    </p>
+
+    <p class="text">
+        <a href="https://github.com/SmartDude04/color-wars-website" target="_blank" class="link">GitHub Repo</a>
+    </p>
+</div>
