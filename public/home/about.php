000632a20826374d6abf82a7a04a0070045812a4
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-07-20 14:29:36 -0400

    Resolve #13

--- public/home/about.php
+++ public/home/about.php
@@ -1,24 +1,24 @@
 <div class="view">
     <img src="img/color-wars-title.png" alt="Logo" class="logo">
     <p class="text">
-        As I was looking for a coding project to keep up my skills over the summer, I thought of creating a
-        website to keep track of color wars scores. After realizing a fully-functional website would be quite
-        a challenge, I  quickly got to work. Thankfully, I can say (with some bias) that I have succeeded in creating
-        a website that can help facilitate all that is needed to manage color wars at CGC: viewing points, managing points,
-        managing colors and groups, and storing user accounts for those who need it. Aside from being fun to make, my
-        hope is that this website can make the management, accuracy, and viewing of color wars material for all who
-        want to do so much easier.
+        This website was created as a side-project for me to keep up my programming skills over the summer. Thankfully,
+        my work here won't sit on a folder as so many projects do, and instead this website's purpose is to streamline
+        the Color Wars process by making it much simpler to manage the week's fun-filled events. Hopefully you find some
+        use in it!
         <br/>
         <br/>
         &emsp;&emsp;- Aidan O'Connor, Creator
+        <br/>
+        &emsp;&emsp;&nbsp;&nbsp;<i>Specialty Camp Lead Counselor</i>
     </p>
 
     <p class="text">
         As this is still an ongoing project, issues may arise. If you encounter an issue, bug, or other general
-        problem, please contact me so I can get it resolved quickly.
+        problem, please contact me, so I can get it resolved quickly.
     </p>
 
     <p class="text">
+        And, for anyone interested:
         <a href="https://github.com/SmartDude04/color-wars-website" target="_blank" class="link">GitHub Repo</a>
     </p>
 </div>
