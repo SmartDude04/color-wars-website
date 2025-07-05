f72ff2006564e7b3029934512fd90c37e0225323
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2025-07-05 10:25:34 -0400

    Add Vercel analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import React from "react";
 import { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
@@ -13,6 +15,8 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
         <html lang="en" suppressHydrationWarning>
             <body className="antialiased">
                 <ThemeProvider>{children}</ThemeProvider>
+                <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
