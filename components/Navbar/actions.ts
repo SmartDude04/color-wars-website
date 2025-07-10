41b07d55a1df81e3397ca5b5b9499d44de2ce3c0
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2025-07-10 19:23:36 -0400

    Fix failure of signout to redirect to home page

--- components/Navbar/actions.ts
+++ components/Navbar/actions.ts
@@ -1,9 +1,9 @@
 "use server";
 
 import { signOut } from "@/auth";
-import { redirect } from "next/navigation";
 
 export async function handleSignOut() {
-    await signOut();
-    redirect("/");
+    await signOut({
+        redirectTo: "/"
+    });
 }
