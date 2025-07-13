5ec4e447f418eb8f6e90fd43050229367b2b09c2
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2025-07-13 10:12:58 -0400

    Change bg color for Add Points

--- components/Navbar/DesktopNavbarLinks.tsx
+++ components/Navbar/DesktopNavbarLinks.tsx
@@ -23,7 +23,7 @@ export default async function DesktopNavbarLinks() {
             { role === "admin" || role === "specialist" ? (
                 <>
                     <Link href="/users" className={linkStyles}>Users</Link>
-                    <Link href="/add-points" className="text-2xl ml-2 mr-2 bg-yellow-400 dark:bg-yellow-950 rounded-4xl pt-2 pb-2 pl-5 pr-5 duration-200">Add Points</Link>
+                    <Link href="/add-points" className="text-2xl ml-2 mr-2 bg-yellow-400 dark:bg-yellow-800 rounded-4xl pt-2 pb-2 pl-5 pr-5 duration-200">Add Points</Link>
                 </>
             ) : <></> }
         </div>
