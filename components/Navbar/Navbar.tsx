28d4013a5be0310fb5f90079f19d2351a73fda1d
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2025-07-18 20:53:58 -0400

    Resolve #7

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import ThemeSwitcher from "@/components/ThemeSwitcher";
 import NavbarLogo from "@/components/Navbar/NavbarLogo";
 import DesktopNavbarLinks from "@/components/Navbar/DesktopNavbarLinks";
 import MobileNavbarLinks from "@/components/Navbar/MobileNavbarLinks";
+import { Suspense } from "react";
 
 export default function Navbar() {
     return (
@@ -10,17 +11,23 @@ export default function Navbar() {
             <div className="flex flex-row items-center">
                 <NavbarLogo />
                 <div className="hidden lg:flex flex-row items-center">
-                    <DesktopNavbarLinks />
+                    <Suspense fallback={null}>
+                        <DesktopNavbarLinks />
+                    </Suspense>
                 </div>
             </div>
             <div className="hidden lg:flex flex-row items-center">
-                <DesktopNavbarAuth/>
+                <Suspense fallback={null}>
+                    <DesktopNavbarAuth/>
+                </Suspense>
                 <div className="ml-2">
                     <ThemeSwitcher />
                 </div>
             </div>
             <div className="flex lg:hidden flex-row items-center">
-                <MobileNavbarLinks />
+                <Suspense fallback={null}>
+                    <MobileNavbarLinks />
+                </Suspense>
             </div>
         </nav>
     );
