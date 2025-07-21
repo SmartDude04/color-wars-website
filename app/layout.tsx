61533e5f80b2e2a266e4e4449aaa609925c53a79
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2025-07-21 18:59:57 -0400

    Change back sample rate of speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
                     </SessionProvider>
                 </ThemeProvider>
                 <Analytics />
-                <SpeedInsights sampleRate={0.2}/>
+                <SpeedInsights />
             </body>
         </html>
     );
