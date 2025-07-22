2579e836d909ba3cf397a7fbbed5c1e865b02b24
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2025-07-21 20:04:37 -0400

    Update UI on signup and login, and remove animate duration from add points

--- app/add-points/page.tsx
+++ app/add-points/page.tsx
@@ -19,7 +19,7 @@ export default async function Page() {
 
         return (
             <div className={`bg-white w-full min-h-[calc(100dvh-72px)] flex justify-center items-center bg-contain bg-center bg-no-repeat`} style={{ backgroundImage: `url(${colorSplashBackground.src})` }}>
-                <div className="w-full min-h-[calc(100dvh-72px)] flex justify-center items-center bg-[rgba(0,0,0,0.2)] dark:bg-[rgba(0,0,0,0.7)] duration-200">
+                <div className="w-full min-h-[calc(100dvh-72px)] flex justify-center items-center bg-[rgba(0,0,0,0.2)] dark:bg-[rgba(0,0,0,0.7)]">
                     <div className="w-[min(600px,90%)] bg-[rgba(255,255,255,0.6)] dark:bg-[rgba(0,0,0,0.2)] dark:border-[#5c5c5c] border-gray-300 border-2 z-0 backdrop-blur-sm shadow-lg flex flex-col justify-center items-center rounded-3xl pl-4 pr-4 pt-8 pb-12 md:p-10 mt-8 mb-8">
                         <h1 className="text-5xl pt-8 pb-2 md:pb-6">Add Points</h1>
                         <AddPointsForm colors={colors} groups={groups} activities={activities} />
